5bc21d7ecac8976763da80b1e3e9edc49c75bd65
Peter Volf <do.volfp@gmail.com>
2024-03-22 18:24:44 +0100

    added all(), one(), and one_or_none() utility methods + tests

--- sqlmodelservice/__init__.py
+++ sqlmodelservice/__init__.py
@@ -1,4 +1,5 @@
 from .service import CommitFailed as CommitFailed
+from .service import MultipleResultsFound as MultipleResultsFound
 from .service import NotFound as NotFound
 from .service import Service as Service
 from .service import ServiceException as ServiceException
